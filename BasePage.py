362c8e0daab92d7a4db7251181db805f355cc11f
ssexec <vasnev2016@yandex.ru>
2023-02-07 11:12:09 +0300

    BasePage и локаторы как селф

---
+++ BasePage.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Browser:
+    def __init__(self,driver):
+        self.driver=driver
+        self.base_url = "http://testfire.net/feedback.jsp"
+class BasePage: #create class for a base page we are going to work with
+
+
+    def find_element(self, locator, time = 10): #creates special element finding function
+        return WebDriverWait(self.driver,time).until(EC.presence_of_element_located(locator),
+                                                      message=f"Can't find element by locator {locator}") #with imported webdriverwait function waits till it finds locator element
+    def go_to_site(self):
+        return self.driver.get(self.base_url) #returns the base_url value
+
+    def return_current_url(self,driver): #function returns current url
+        check_url=driver.current_url #with driver current_url function saves value in check_url variable
+        return check_url
