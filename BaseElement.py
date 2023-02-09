853aea267b4ab0bbcb7d41e64c000423475dc7b9
ssexec <vasnev2016@yandex.ru>
2023-02-09 20:10:55 +0300

    EdgeBrowser

--- BaseElement.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-class Locator():
-    locType = By.NAME
-    SEARCH_FIELD_YOUR_NAME = (locType, "name")  # creates locator for Your Name field
-    SEARCH_FIELD_YOUR_EMAIL = (locType, "email_addr")  # creates locator for Your Email Address field
-    SEARCH_SUBJECT = (locType, "subject")  # creates locator for YSubject field
-    SEARCH_COMMENTS = (locType, "comments")  # creates locator for Question/Comment field
-    SUBMIT_BUTTON = (locType, "submit")  # creates locator for submit button
--- test_start.py
+++
@@ -1,9 +0,0 @@
-from TestFunc import Tests
-
-def test_altoro_feedback_only_name(browser,mainPage=Tests()): #tests if we can't send data without filling other important fields
-    result = mainPage.func(browser, 'Sergey',t=2)
-    assert result
-#1234567890
-def test_altoro_feedback_JS(browser,mainPage=Tests()): #test whether we can call for a JS alert window through Your Name field
-    result = mainPage.func(browser,"<script>alert('attack')</script>",t=1)
-    assert result
